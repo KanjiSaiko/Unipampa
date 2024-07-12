080d36aa4a3c65d44b4a5abef7cee33bf091b050
KanjiSaiko <135777019+KanjiSaiko@users.noreply.github.com>
2024-07-12 14:59:00 -0300

    Delete SPOTYFOM directory

--- SPOTYFOM/criaNodo.c
+++
@@ -1,9 +0,0 @@
-#include "bibliotecas.h"
-
-
-NodoLP *criaNodo(){
-    NodoLP *nodo = malloc(sizeof(NodoLP));
-    nodo->info = NULL;
-    nodo->prox = NULL;
-    return nodo;
-}
--- SPOTYFOM/inicializaLE.c
+++
@@ -1,8 +0,0 @@
-#include "bibliotecas.h"
-
-DescLE *inicializaLE(){
-    DescLE *descritorLE = malloc(sizeof(DescLE));
-    descritorLE->primeiro = NULL;
-    descritorLE->tamanho = 0;
-    return descritorLE;
-}
